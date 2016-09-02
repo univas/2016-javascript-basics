897708586d897f76dbcb3c000326611177fdd2fb
Edy Segura <edysegura@gmail.com>
2016-09-01 21:55:58 -0300

    Functional sum.js

--- sum/sum.js
+++ sum/sum.js
@@ -1,4 +1,4 @@
-var numberA = process.argv[2];
-var numberB = process.argv[3];
+var numberA = parseFloat(process.argv[2]);
+var numberB = parseFloat(process.argv[3]);
 
 console.log("Total: " + (numberA + numberB));
