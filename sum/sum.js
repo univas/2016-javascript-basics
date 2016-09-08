00852e049c97b31d7c2b63d49648cdc5083229b6
Edy Segura <edysegura@gmail.com>
2016-09-08 20:12:59 -0300

    Added method getNumbers()

--- sum/sum.js
+++ sum/sum.js
@@ -1,4 +1,14 @@
-var numberA = parseFloat(process.argv[2]);
-var numberB = parseFloat(process.argv[3]);
 
-console.log("Total: " + (numberA + numberB));
+console.log("Numbers: " + getNumbers());
+
+function getNumbers() {
+    var numbers = [];
+    for (var i = 2; i < process.argv.length; i++) {
+        var stringNumber = process.argv[i];
+        var number = parseFloat(stringNumber);
+        if(!isNaN(number)) {
+            numbers.push(number);
+        }
+    }
+    return numbers;
+}
